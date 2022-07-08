4275de9b1ed1ca9e58bb78a292640a02b1e329df
Max <max@seervision.com>
2022-07-08 18:56:51 +0200

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+ARG ROS_DISTRO
+
+FROM ros:${ROS_DISTRO}-ros-core
+
+WORKDIR /colcon_ws
+
+RUN apt update && apt install -y --no-install-recommends python3-rpi.gpio python3-pip python3-colcon-common-extensions && rm -rf /var/lib/apt/lists/*
+RUN pip3 install adafruit-circuitpython-lis3dh
+
+COPY accelerometer_publisher . 
+RUN . /opt/ros/${ROS_DISTRO}/setup.sh && colcon build
+
+WORKDIR /
+
+COPY ros_entrypoint.sh .
---
+++ accelerometer_publisher/accelerometer_publisher/publisher_member_function.py
@@ -0,0 +1,53 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+
+import board
+import adafruit_lis3dh
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.i2c = board.I2C()
+        self.lis3dh = adafruit_lis3dh.LIS3DH_I2C(self.i2c)
+        self.publisher_ = self.create_publisher(Float32, 'topic', 10)
+        timer_period = 0.2  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        x, y, z = self.lis3dh.acceleration
+        msg = Float32(data=x)
+        self.publisher_.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
